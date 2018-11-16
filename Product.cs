00cd25db524a92c61eb9e1590bf7ae0daf77d333
Ben Jen <benjendrzejak@quickenloans.com>
2018-11-15 21:49:09 -0500

    Added inventory list

--- Product.cs
+++ Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,16 @@ namespace Midterm2
         public string Category { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
+
+
+
+        public static void GetInventoryList()
+        {
+            using (StreamReader file =
+                new StreamReader(@"C:\dev\GrandCircus\Midterm2\inventory.txt"))
+            {
+                //Console.WriteLine(file.ReadToEnd());
+            }
+        }
     }
 }
